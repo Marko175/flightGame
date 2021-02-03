88be3b53367fffdfa8f12209640afa3cb8a1c7fa
Marko175 <78277702+Marko175@users.noreply.github.com>
2021-02-03 15:51:45 +0200

    Fix bug horizonlines
    Now all we need is to complete the rest of the lines

--- f16/Assets/Scripts/HUD.cs
+++ f16/Assets/Scripts/HUD.cs
@@ -73,63 +73,55 @@ public class HUD : MonoBehaviour
         FPM.position = Camera.main.WorldToScreenPoint(velocityPos);
 
 
-        Horizon.position = Camera.main.WorldToScreenPoint(player.transform.position + Vector3.Project(player.transform.forward, Quaternion.Euler(0,0,0)*new Vector3(player.transform.forward.x,0,player.transform.forward.z)).normalized * scope);
-        Horizon.rotation =  Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-
-        GenerateSemiHorizon(player);
+        GenerateHorizon(player);
 
         
         g.text = $"{player.g:0.0}G";
-        Debug.Log(player.transform.rotation.x);
+
 
     }
 
-    private void GenerateSemiHorizon(sufa player)
+    private void GenerateHorizon(sufa player)
     {
-        float deg = -5;
-        five.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized*Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope;
+        Horizon.position = Camera.main.WorldToScreenPoint(player.transform.position + Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * scope);
+        Horizon.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        float deg = 5;
+        five.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0,0,0)*(new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized* Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         five.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        five.position = Camera.main.WorldToScreenPoint(five.position);
+
 
         deg = 10;
-        ten.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope;
+        ten.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         ten.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        ten.position = Camera.main.WorldToScreenPoint(ten.position);
 
         deg = 15;
-        fifteen.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg) ,0))* scope;
+        fifteen.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         fifteen.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        fifteen.position = Camera.main.WorldToScreenPoint(fifteen.position);
 
         deg = 20;
-        twenty.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope;
+        twenty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         twenty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        twenty.position = Camera.main.WorldToScreenPoint(twenty.position);
 
         deg = 25;
-        twentyfive.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope;
+        twentyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         twentyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        twentyfive.position = Camera.main.WorldToScreenPoint(twentyfive.position);
 
         deg = 30;
-        thirty.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope;
+        thirty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         thirty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        thirty.position = Camera.main.WorldToScreenPoint(thirty.position);
 
         deg = 35;
-        thirtyfive.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope;
+        thirtyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         thirtyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        thirtyfive.position = Camera.main.WorldToScreenPoint(thirtyfive.position);
 
         deg = 40;
-        fourty.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope;
+        fourty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         fourty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        fourty.position = Camera.main.WorldToScreenPoint(fourty.position);
 
         deg = 45;
-        fourtyfive.position = player.transform.position + Vector3.Project(player.transform.forward, (new Vector3(player.transform.forward.x, 0, player.transform.forward.z)).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope;
+        fourtyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
         fourtyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
-        fourtyfive.position = Camera.main.WorldToScreenPoint(fourtyfive.position);
 
     }
 }
