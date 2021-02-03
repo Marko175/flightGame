41b7f2ba89ccc1d4a0f2b7c46fc0bd6e36781717
Marko175 <78277702+Marko175@users.noreply.github.com>
2021-02-03 19:50:07 +0200

    finished Hud
    smaller hud with all horizon lines posative and negative

--- f16/Assets/Scripts/HUD.cs
+++ f16/Assets/Scripts/HUD.cs
@@ -58,6 +58,35 @@ public class HUD : MonoBehaviour
     [SerializeField] private RectTransform eighty_over = null;
     [SerializeField] private RectTransform eightyfive_over = null;
 
+    [Header("NegSemiHorizon")]
+    [SerializeField] private RectTransform nvertical = null;
+    [SerializeField] private RectTransform nfive = null;
+    [SerializeField] private RectTransform nten = null;
+    [SerializeField] private RectTransform nfifteen = null;
+    [SerializeField] private RectTransform ntwenty = null;
+    [SerializeField] private RectTransform ntwentyfive = null;
+    [SerializeField] private RectTransform nthirty = null;
+    [SerializeField] private RectTransform nthirtyfive = null;
+    [SerializeField] private RectTransform nfourty = null;
+    [SerializeField] private RectTransform nfourtyfive = null;
+    [SerializeField] private RectTransform nfifty = null;
+    [SerializeField] private RectTransform nfiftyfive = null;
+    [SerializeField] private RectTransform nsixty = null;
+    [SerializeField] private RectTransform nsixtyfive = null;
+    [SerializeField] private RectTransform nseventy = null;
+    [SerializeField] private RectTransform nseventyfive = null;
+    [SerializeField] private RectTransform neighty = null;
+    [SerializeField] private RectTransform neightyfive = null;
+
+
+    [Header("NegSemiHorizon_Over")]
+    [SerializeField] private RectTransform nsixty_over = null;
+    [SerializeField] private RectTransform nsixtyfive_over = null;
+    [SerializeField] private RectTransform nseventy_over = null;
+    [SerializeField] private RectTransform nseventyfive_over = null;
+    [SerializeField] private RectTransform neighty_over = null;
+    [SerializeField] private RectTransform neightyfive_over = null;
+
 
 
     // Start is called before the first frame update
@@ -91,11 +120,124 @@ public class HUD : MonoBehaviour
 
 
         GenerateHorizon(player);
+        GenerateNegHorizon(player);
 
         
         g.text = $"{player.g:0.0}G";
 
 
+    }
+
+    private void GenerateNegHorizon(sufa player)
+    {
+
+
+        float deg = -5;
+        nfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -10;
+        nten.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nten.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -15;
+        nfifteen.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nfifteen.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -20;
+        ntwenty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        ntwenty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -25;
+        ntwentyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        ntwentyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -30;
+        nthirty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nthirty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -35;
+        nthirtyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nthirtyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -40;
+        nfourty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nfourty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -45;
+        nfourtyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nfourtyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -50;
+        nfifty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nfifty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -55;
+        nfiftyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nfiftyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        deg = -60;
+        nsixty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nsixty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        nsixty_over.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * -Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nsixty_over.rotation = Quaternion.Euler(0, 0, 180 - player.transform.rotation.eulerAngles.z);
+
+        deg = -65;
+        nsixtyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nsixtyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        nsixtyfive_over.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * -Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nsixtyfive_over.rotation = Quaternion.Euler(0, 0, 180 - player.transform.rotation.eulerAngles.z);
+
+        deg = -70;
+        nseventy.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nseventy.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        nseventy_over.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * -Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nseventy_over.rotation = Quaternion.Euler(0, 0, 180 - player.transform.rotation.eulerAngles.z);
+
+        deg = -75;
+        nseventyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nseventyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        nseventyfive_over.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * -Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nseventyfive_over.rotation = Quaternion.Euler(0, 0, 180 - player.transform.rotation.eulerAngles.z);
+
+        deg = -80;
+        neighty.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        neighty.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        neighty_over.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * -Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        neighty_over.rotation = Quaternion.Euler(0, 0, 180 - player.transform.rotation.eulerAngles.z);
+
+        deg = -85;
+        neightyfive.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        neightyfive.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        neightyfive_over.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * -Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        neightyfive_over.rotation = Quaternion.Euler(0, 0, 180 - player.transform.rotation.eulerAngles.z);
+
+        deg = -90;
+        nvertical.position = Camera.main.WorldToScreenPoint(player.transform.position + (Vector3.Project(player.transform.forward, Quaternion.Euler(0, 0, 0) * (new Vector3(player.transform.forward.x, 0, player.transform.forward.z))).normalized * Mathf.Cos(Mathf.Deg2Rad * deg) + new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * deg), 0)) * scope);
+        nvertical.rotation = Quaternion.Euler(0, 0, -player.transform.rotation.eulerAngles.z);
+
+        if (player.transform.forward.y > 0)
+        {
+            nsixty.position = nthirty.position;
+            nsixty_over.position = nthirty.position;
+            nsixtyfive.position = nthirty.position;
+            nsixtyfive_over.position = nthirty.position;
+            nseventy.position = nthirty.position;
+            nseventy_over.position = nthirty.position;
+            nseventyfive.position = nthirty.position;
+            nseventyfive_over.position = nthirty.position;
+            neighty.position = nthirty.position;
+            neighty_over.position = nthirty.position;
+            neightyfive.position = nthirty.position;
+            neightyfive_over.position = nthirty.position;
+            nvertical.position = nthirty.position;
+        }
     }
 
     private void GenerateHorizon(sufa player)
